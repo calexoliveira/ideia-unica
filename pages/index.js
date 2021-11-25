235db4e070dc43f98fa7694b1a523d9047ea4332
Claudio Alex de Oliveira <calexoliveira@gmail.com>
2021-11-25 00:39:25 -0300

    teste link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 function Home(){
-    return <h4>Página Inicial</h4>
+    return(
+            <div>
+              <h1>Home</h1>
+              <Link href="/sobre">
+              <a>Acessar página Sobre</a>
+              </Link>
+            </div>
+        )
 }
 
 
