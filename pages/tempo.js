c9a2458f6f3f775d11c8101e80e792e1236d8533
Claudio Alex de Oliveira <calexoliveira@gmail.com>
2021-11-28 14:26:12 -0300

    teste getStaticProps

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,5 +1,5 @@
 function Tempo(props){
-
+    console.log('> Passando pelo Frontend();');
         const dynamicDate = new Date();
         const dynamicDateString = dynamicDate.toGMTString();
 
@@ -13,6 +13,7 @@ function Tempo(props){
 }
 
 export function getStaticProps(){
+        console.log('> Passando pelo getStaticProps();');
         const staticDate = new Date();
         const staticDateString = staticDate.toGMTString();
 
